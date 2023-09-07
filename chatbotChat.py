7711b509f0fbc1b67b44aeb44474c6cf79f727ff
Hyacinthax <49726913+hyacinthhax@users.noreply.github.com>
2023-09-06 18:32:12 -0600

    Update chatbotChat.py
    Fixed load_model to remove parameters which were unecesarry

--- chatbotChat.py
+++ chatbotChat.py
@@ -1,38 +1,37 @@
 import re
 import numpy as np
 import pickle
-from chatbot_trainer import ChatbotTrainer  # Import the ChatbotTrainer class
+from chatbotTrainer import ChatbotTrainer  # Import the ChatbotTrainer class
 
-def preprocess_text(text):
-    cleaned_text = text.replace('"', '')
-    cleaned_text = re.sub(r"\s+", " ", cleaned_text).strip()
-    return cleaned_text.lower()
 
-if __name__ == "__main__":
-    model_path = "chatbot_model.h5"
-    max_seq_length = 100
+
+
+model_path = "chatbot_model.h5"
+max_seq_length = 100
+chatbot_trainer = ChatbotTrainer()
+
+# Load the saved tokenizer
+tokenizer_load_path = "chatBotTokenizer.pkl"  # Update with the actual path to your saved tokenizer
+with open(tokenizer_load_path, 'rb') as tokenizer_load_file:
+    loaded_tokenizer = pickle.load(tokenizer_load_file)
+    chatbot_trainer.tokenizer = loaded_tokenizer
+    print("Number of words in loaded tokenizer:", len(chatbot_trainer.tokenizer.word_index))
+
+
+chatbot_trainer.load_model()
+print("Chatbot is ready. Type 'exit' to end the conversation.")
+
+
+while True:
+    user_input = input("You: ")
+    if user_input.lower() == "exit":
+        print("Chatbot: Goodbye!")
+        break
     
-    # Load the saved tokenizer
-    tokenizer_load_path = "chatBotTokenizer.pkl"  # Update with the actual path to your saved tokenizer
-    with open(tokenizer_load_path, 'rb') as tokenizer_load_file:
-        loaded_tokenizer = pickle.load(tokenizer_load_file)
-        print("Number of words in loaded tokenizer:", len(loaded_tokenizer.word_index))
+    input_text = chatbot_trainer.preprocess_text(user_input)
+    if not input_text:
+        print("Chatbot: I'm sorry, I don't understand your input.")
+        continue
     
-    chatbot_trainer = ChatbotTrainer()
-    chatbot_trainer.load_model(model_path, loaded_tokenizer, max_seq_length)
-    
-    print("Chatbot is ready. Type 'exit' to end the conversation.")
-    
-    while True:
-        user_input = input("You: ")
-        if user_input.lower() == "exit":
-            print("Chatbot: Goodbye!")
-            break
-        
-        input_text = preprocess_text(user_input)
-        if not input_text:
-            print("Chatbot: I'm sorry, I don't understand your input.")
-            continue
-        
-        response = chatbot_trainer.generate_response(input_text)
-        print(f"Chatbot: {response}")
+    response = chatbot_trainer.generate_response(input_text)
+    print(f"Chatbot: {response}")
