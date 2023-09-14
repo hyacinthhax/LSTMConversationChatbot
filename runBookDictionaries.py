d7ad0573d9c733632652396ac8ce9a381eb33289
Hyacinthax <49726913+hyacinthhax@users.noreply.github.com>
2023-09-14 15:29:17 -0700

    Update runBookDictionaries.py
    Changes tokenizer function for chatbotTrainer changes(Moved tokenizer saving to function)

--- runBookDictionaries.py
+++ runBookDictionaries.py
@@ -10,15 +10,11 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import logging
 import pickle
-  # Import the dialog_data dictionary
-from playsound import playsound
-from chatbotTrainer import ChatbotTrainer  # Import your ChatbotTrainer class
 import nltk
 from nltk.tokenize import sent_tokenize
-
+from chatbotTrainer import ChatbotTrainer  # Import your ChatbotTrainer class
 
 nltk.download('punkt')  # Download the sentence tokenizer model if not already installed
-
 
 def book_to_dict(book_filename):
     print("Making Dictionary...  ")
@@ -42,17 +38,29 @@ def main():
     print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
     chatbot_trainer = ChatbotTrainer()
 
-    bookName = ""
-    if bookName == "":
-        bookName = input("(Put in folder with runBookDictionaries.py)  \n(SomeBookCalled.txt)  \n>  ")
-    dialog_data = book_to_dict(bookName)
+    # Initialize the tokenizer
+    if os.path.exists(chatbot_trainer.tokenizer_save_path):
+        with open(chatbot_trainer.tokenizer_save_path, 'rb') as tokenizer_load_file:
+            chatbot_trainer.tokenizer = pickle.load(tokenizer_load_file)
+            chatbot_trainer.tokenizer.num_words = chatbot_trainer.max_vocab_size
+            chatbot_trainer.logger.info("Model and tokenizer loaded successfully.")
+    else:
+        print("Tokenizer not found, making now...  ")
+        chatbot_trainer.tokenizer = Tokenizer(oov_token="<OOV>", num_words=chatbot_trainer.max_vocab_size)  # Initialize the Tokenizer
+        chatbot_trainer.tokenizer.num_words = chatbot_trainer.max_vocab_size
 
-    # Once all book name data is processed, you can fit the tokenizer
+    # Prompt the user for the book name
+    bookName = input("(Put in folder with runBookDictionaries.py)\n(SomeBookCalled.txt)\n> ")
+
+    # Process book data and create the dialog_data dictionary
+    dialog_data = book_to_dict(bookName)
+    
+    # Once all book data is processed, you can fit the tokenizer
     all_input_texts = [chatbot_trainer.preprocess_text(pair[0]) for pairs in dialog_data.values() for pair in pairs]
     all_target_texts = [chatbot_trainer.preprocess_text(pair[1]) for pairs in dialog_data.values() for pair in pairs]
-    train_input_texts, test_input_texts, train_target_texts, test_target_texts = train_test_split(all_input_texts, all_target_texts, test_size=0.2, random_state=42)
-
-    chatbot_trainer.tokenizer.fit_on_texts(train_input_texts + train_target_texts)
+    
+    # Fit the tokenizer on all book data
+    chatbot_trainer.tokenizer.fit_on_texts(all_input_texts + all_target_texts)
 
     # Train models for each book
     for bookName, sentences in dialog_data.items():
