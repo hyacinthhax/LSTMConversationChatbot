cad2b2bb56ebc7bfe12a4a6670af045cf401ede6
Hyacinthax <49726913+hyacinthhax@users.noreply.github.com>
2023-09-14 18:51:12 -0700

    Update setup_chatbot.py
    Added additional instruction in print function

--- setup_chatbot.py
+++ setup_chatbot.py
@@ -4,7 +4,7 @@ def setup_chatbot():
     # Install dependencies using pip
     os.system('pip install -r requirements.txt')
 
-    print("Setup completed. You can now run the chatbot with 'python runMovieCorpus.py'.")
+    print("Setup completed. You can now run the chatbot with 'python runMovieCorpus.py'.  \nREMEMBER TO PREPROCESS USING preprocess_dialogs.py  \n")
 
 if __name__ == "__main__":
     setup_chatbot()
